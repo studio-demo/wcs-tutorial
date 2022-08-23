2b1b8db1592f8b45193c05b44fe17df0e1b62049
Ari Kishon <ariki@wix.com>
2022-08-23 16:35:09 +0300

    fix: update text

--- src/components/tasks/10/task-10.tsx
+++ src/components/tasks/10/task-10.tsx
@@ -14,10 +14,10 @@ export const Task10: React.FC<Task10Props> = (className) => {
       </div>
       <div className={styles.title}>Work with Git</div>
       <ol start={0} className={styles.desc}>
-        <li> WCS is using <b>Git</b> for version control. This means you can manage different versions of your projects through <b>branches</b>. Let’s switch to one with solved lessons. </li>
+        <li> WCS is using <b>Git</b> for version control. This means you can manage different versions of your projects through <b>branches</b>. Let’s switch to one with the lessons solved. </li>
         <li> Before switching branches, you have to <b>commit</b> or <b>revert</b> your changes (if you have any). Commit them to your current branch with <TaskSymbol name="commit" />. Alternatively, revert them in the <TaskSymbol name="more" /> menu at the top right. </li>
         <li> When the time comes, you'll need a <b>GitHub</b> account to publish or <b>sync</b> your project. Just click <TaskSymbol name="sync" /> </li>  
-        <li> Time to switch. Use <TaskSymbol name="branch" /> and switch to <b>solved-lessons</b> branch. Enjoy the solved lessons!</li>
+        <li> Time to switch. Use <TaskSymbol name="branch" /> and switch to the <b>solved-lessons</b> branch. Enjoy!</li>
       </ol>
     </div>
   );
